42468f095efb3d14af0dbccca3c212320ced5b7a
matsouto <matsouto55@gmail.com>
2023-12-20 23:20:27 -0300

    load with bogo3

--- flights_dag.py
+++ flights_dag.py
@@ -1,7 +1,13 @@
 from datetime import datetime, timedelta
+from operator import index
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from flights_etl import run_flights_etl
+from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
+
+from FlightRadar24 import FlightRadar24API
+import pandas as pd
+import logging
+import boto3
 
 default_args = {
     "owner": "matsouto",
@@ -21,10 +27,113 @@ dag = DAG(
 )
 
 
-run_etl = PythonOperator(
-    task_id="complete_flights_etl",
-    python_callable=run_flights_etl,
+def extract_flight_data(**kwargs):
+    # ------- Flightradar API --------
+    api = FlightRadar24API()
+
+    # Configuring the api
+    flight_tracker = api.get_flight_tracker_config()
+    flight_tracker.limit = 1500
+    api.set_flight_tracker_config(flight_tracker)
+
+    # ------- Flights --------
+    # Getting all Embraer commercial jets flights
+    def get_flights(aircraft_type: str):
+        # Gets all the flights for a specific aircraft
+        return api.get_flights(aircraft_type=aircraft_type)
+
+    embraer_arcrafts = [
+        # "E170",
+        # "E75L",
+        # "E75S",
+        # "E190",
+        # "E290",
+        "E195",
+        "E295",
+        "E135",
+        "E145",
+    ]
+    # Creating a dictionary for the flights
+    flights_dict = {}
+    for aircraft_type in embraer_arcrafts:
+        flights = get_flights(aircraft_type)
+        flights_dict[aircraft_type] = flights
+
+    # Appends each flight to a list
+    all_flights = []
+    for aircraft_type in embraer_arcrafts:
+        for flight in flights_dict[aircraft_type]:
+            # Updates some informations from the flight
+            try:
+                flight_details = api.get_flight_details(flight)
+                flight.set_flight_details(flight_details)
+            except Exception as e:
+                pass
+
+            # Creating the object of the flight
+            _dict = {
+                "flight_id": flight.id,
+                "heading": flight.heading,
+                "altitude": flight.altitude,
+                "ground_speed": flight.ground_speed,
+                "origin_airport": flight.origin_airport_iata,
+                "destination_airport": flight.destination_airport_iata,
+                "airline_iata": flight.airline_iata,
+                "onground": flight.on_ground,
+                "latitude": flight.latitude,
+                "longitude": flight.longitude,
+                "aircraft_model": getattr(flight, "aircraft_model", "N/A"),
+                "aircraft_code": flight.aircraft_code,
+                "aircraft_country_id": getattr(flight, "aircraft_country_id", "N/A"),
+                "aircraft_type": aircraft_type,
+                "time": flight.time,
+            }
+            logging.info(f"Extraction completed for {flight.id}.")
+            all_flights.append(_dict)
+
+    # Generate the CSV file
+    flights_df = pd.DataFrame(all_flights)
+
+    # Replace 'N/A' with NaN
+    # This is important to prevent errors in the serialization of XCOM
+    flights_df.replace("N/A", pd.NA, inplace=True)
+
+    # Push data to Airflow XCOM
+    kwargs["ti"].xcom_push(key="extracted_data", value=flights_df)
+
+
+def load_to_s3(**kwargs):
+    # Pull data from Airflow XCOM
+    data = kwargs["ti"].xcom_pull(key="extracted_data")
+    dataframe = pd.DataFrame.from_dict(data)
+
+    # Convert to csv file
+    csv_data = dataframe.to_csv(index=False)
+
+    # Upload the file
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id="AKIA3W53N4VZE3FW26FI",
+        aws_secret_access_key="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",
+    )
+    s3.put_object(
+        Body=csv_data,
+        Bucket="matsouto-flights-elt",
+        Key=f"{datetime.now().strftime('%Y-%m-%d %H:%M')}.csv",
+    )
+    logging.info("File uploaded to S3")
+
+
+extract_task = PythonOperator(
+    task_id="extraction",
+    python_callable=extract_flight_data,
     dag=dag,
 )
 
-run_etl
+load_task = PythonOperator(
+    task_id="load",
+    python_callable=load_to_s3,
+    dag=dag,
+)
+
+extract_task >> load_task
